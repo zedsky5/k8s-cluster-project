60b6d819a9a43e7c28e0d131cf73e5c470956cb6
zedsky5 <zshyikh@gmail.com>
2025-08-07 15:20:17 -0500

    add NAT settings

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,6 +30,9 @@ module "vpc" {
   azs             = ["us-east-2a", "us-east-2b"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.3.0/24", "10.0.4.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
 }
 
 module "eks" {
