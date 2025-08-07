a0fae0559cfbbf4992b3b1d3066c84527ed14ba7
zedsky5 <109177347+zedsky5@users.noreply.github.com>
2025-08-07 12:47:47 -0500

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,8 +7,3 @@ output "eks_cluster_name" {
   description = "The name of the EKS cluster."
   value       = module.eks.cluster_name
 }
-
-output "kubeconfig" {
-  description = "The kubeconfig for the EKS cluster."
-  value       = module.eks.kubeconfig
-}
